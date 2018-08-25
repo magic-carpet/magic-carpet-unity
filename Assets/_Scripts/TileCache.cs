21c59344853f6f00b4be7c06d900ab1e89a9d051
Daniel Ocean <develop@freeidealstudio.com>
2018-08-25 15:08:16 +0100

    Initital setup for panels in campus scene

---
+++ Assets/_Scripts/TileCache.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCache : MonoBehaviour {
+
+    public GameObject[] panels;
+    public Transform[] panelPosn;
+    private GameObject panelParent;
+
+	// Use this for initialization
+	void Start () {
+       // panelParent = this.transfomr
+            }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

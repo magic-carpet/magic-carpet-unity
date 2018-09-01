9a98e68c522d103fd512fd6f6d0b87cc941398c9
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-09-01 12:06:25 +0100

    Changed to unity version 2018.2.3f1 due to bug Profiling of panels in AR tested.

---
+++ Assets/_Scripts/PanelSpin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSpin : MonoBehaviour {
+
+    private Rigidbody _rb;
+    [SerializeField] private float _rotSpeed=100f;
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+        //_rb.AddTorque(Vector3.forward * _rotSpeed);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void SpinPanel(){
+        _rb.AddTorque(Vector3.forward * _rotSpeed);
+
+    }
+}

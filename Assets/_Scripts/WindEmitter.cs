111bf3ce009365f89985d01f677f808ed4e7deb6
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-09-18 19:36:55 +0100

    Setup initial wind emitter

---
+++ Assets/_Scripts/WindEmitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEmitter : MonoBehaviour {
+
+    [SerializeField] private GameObject _windObject;
+    [SerializeField] private GameObject _windEmitterObject;
+    private float _timer;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(BlowWind());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator BlowWind(){
+        //Debug.Log("Blowing Wind");
+        _timer = Random.Range(0.2f, 0.5f);
+        yield return new WaitForSeconds(_timer);
+        Instantiate(_windObject, _windEmitterObject.transform.position, _windEmitterObject.transform.rotation, gameObject.transform);
+        StartCoroutine(BlowWind());
+    }
+}
---
+++ Assets/_Scripts/WindMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float MoveSpeed = 50.0f;
+
+    public float frequency = 40.0f;  // Speed of sine movement
+    public float magnitude = 2.0f;   // Size of sine movement
+    private Vector3 axis;
+
+    private Vector3 pos;
+
+    // Use this for initialization
+    void Start () {
+        //rb = GetComponent<Rigidbody>();
+        transform.localScale = Vector3.one * Random.Range(0.5f, 2.5f);
+        pos = transform.position;
+        Destroy(gameObject, 10.0f);
+        axis = transform.right;  // May or may not be the axis you want
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        pos -= transform.forward * Time.deltaTime * MoveSpeed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Panel"){
+            //Debug.Log("Wind hit " + other.gameObject.name);
+            other.gameObject.GetComponent<PanelSpin>().WindSpinPanel();
+        }
+    }
+}

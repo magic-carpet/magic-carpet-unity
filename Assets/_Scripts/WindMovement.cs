fc0a3d0959e5f50676c50ce73b850ec04499baf5
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-09-20 20:25:59 +0100

    Wind breeze setup - has toggle for strong wind

--- Assets/_Scripts/WindMovement.cs
+++ Assets/_Scripts/WindMovement.cs
@@ -7,21 +7,27 @@ public class WindMovement : MonoBehaviour {
     private Rigidbody rb;
     public bool strongWind = false;
 
-    public float MoveSpeed = 50.0f;
+    public float MoveSpeed = 20.0f;
 
     public float frequency = 40.0f;  // Speed of sine movement
     public float magnitude = 2.0f;   // Size of sine movement
-    private Vector3 axis;
 
     private Vector3 pos;
 
     // Use this for initialization
     void Start () {
         //rb = GetComponent<Rigidbody>();
-        transform.localScale = Vector3.one * Random.Range(0.5f, 2.5f);
+        if (strongWind)
+        {
+            transform.localScale = Vector3.one * Random.Range(1f,3.5f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one * Random.Range(0.5f, 2.5f);
+        }
+       
         pos = transform.position;
-        Destroy(gameObject, 10.0f);
-        axis = transform.right;  // May or may not be the axis you want
+        Destroy(gameObject, 3.0f);
 
     }
 	
@@ -29,7 +35,7 @@ public class WindMovement : MonoBehaviour {
 	void Update () {
 
         pos -= transform.forward * Time.deltaTime * MoveSpeed;
-        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+        transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
     }
 
     private void OnTriggerEnter(Collider other)

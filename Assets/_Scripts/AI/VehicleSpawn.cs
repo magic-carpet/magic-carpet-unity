3a84b7f476a8c127c16980ecc040536d088188dd
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-09-24 19:44:56 +0100

    Misc environ changes

---
+++ Assets/_Scripts/AI/VehicleSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawn : MonoBehaviour {
+
+   public GameObject vehicleGO;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnCars());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator SpawnCars(){
+        yield return new WaitForSeconds(Random.Range(2.0f, 5.0f));
+        var clone = Instantiate(vehicleGO, transform.position, transform.rotation, transform);
+        Destroy(clone, 600f);
+        StartCoroutine(SpawnCars());
+
+    }
+}
